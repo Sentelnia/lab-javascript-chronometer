93a68e897750f06b4ef61bbfbc157cd71e799a7d
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-03-08 14:57:24 +0100

    splitclick

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -4,32 +4,39 @@ class Chronometer {
     this.currentTime = 0;
     this.intervalId = 0;
   }
-  startClick(callback) {
-    // ... your code goes here
-    //let time = this.currentTime;
+  startClick() {
     this.intervalId = setInterval(() => {
       this.currentTime +=1
     },1000);
   }
 
   getMinutes() {
-    // ... your code goes here
     return Math.floor(this.currentTime / 60);
   }
+
   getSeconds() {
-    // ... your code goes here
     return this.currentTime % 60
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  twoDigitsNumber(num) {
+    num = `${num}`;
+    if (num.length <=1) {
+      return `0${num}`
+    }
+    return `${num}`
   }
+
   stopClick() {
-    // ... your code goes here
+    return clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
   splitClick() {
-    // ... your code goes here
+    let minutes = this.twoDigitsNumber(this.getMinutes());
+    let secondes = this.twoDigitsNumber(this.getSeconds());
+    return `${minutes}:${secondes}`
   }
 }
+
+
