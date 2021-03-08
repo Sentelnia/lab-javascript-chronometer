59de47121ce809641f5a695502fe4aa953174f90
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-03-08 12:52:07 +0100

    first iteration

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,15 +1,24 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
     // ... your code goes here
+    //let time = this.currentTime;
+    this.intervalId = setInterval(() => {
+      this.currentTime +=1
+    },1000);
   }
+
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60
   }
   twoDigitsNumber() {
     // ... your code goes here
